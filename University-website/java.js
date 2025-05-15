71773a4c63f5150e59b97f1cc3c9182acdef01be
kusum219 <mgkusum55@gmail.com>
2025-05-16 00:14:01 +0545

    Add files via upload

---
+++ University-website/java.js
@@ -0,0 +1,11 @@
+//for toogling nav-bar in mobile
+const navLinks = document.getElementById('nav-links');
+
+function showMenu() {
+    navLinks.style.display = "block"; // Show the menu
+}
+
+function hideMenu() {
+    navLinks.style.display = "none"; // Hide the menu
+}
+
